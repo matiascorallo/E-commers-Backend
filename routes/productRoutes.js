aeb03fc39de4eb69db0f9247cfdfe21eac226063
Matias Corallo <matiascorallodev@gmail.com>
2023-02-24 13:58:21 -0300

    problemas con las categorias

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Product = require('../Models/Product');
 const User = require('../Models/user');
 
-router.get('/', async(req, res)=> {
+router.get('/', async (req, res) => {
     try {
         const products = await Product.find();
         res.status(200).json(products)
@@ -14,8 +14,8 @@ router.get('/', async(req, res)=> {
 
 router.post('/', async (req, res) => {
     try {
-        const {name, description, price, category, images: pictures} = req.body;
-        const product = await Product.create({name, description, price, category, pictures})
+        const { name, description, price, category, images: pictures } = req.body;
+        const product = await Product.create({ name, description, price, category, pictures })
         const products = await Product.find();
         res.status(201).json(products);
     } catch (e) {
@@ -23,12 +23,12 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.patch('/:id', async(req, res)=> {
-    const {id} = req.params;
+router.patch('/:id', async (req, res) => {
+    const { id } = req.params;
     try {
-        const{} = req.params;
-        const {name, description, price, category, images: pictures} = req.body;
-        const product = await Product.findByIdAndUpdate(id, {name, description, price, category, pictures});
+        const { } = req.params;
+        const { name, description, price, category, images: pictures } = req.body;
+        const product = await Product.findByIdAndUpdate(id, { name, description, price, category, pictures });
         const products = await Product.find();
         res.status(200).json(products);
     } catch (e) {
@@ -37,28 +37,46 @@ router.patch('/:id', async(req, res)=> {
 })
 
 
-router.delete('/:id', async(req, res) => {
-    const {id} = req.params;
-    const {user_id} = req.body;
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { user_id } = req.body;
     try {
         const user = await User.findById(user_id);
-        if(!user.isAdmin) return res.status(401).json("You don´t have permission");
+        if (!user.isAdmin) return res.status(401).json("You don't have permission");
         await Product.findByIdAndDelete(id);
         const products = await Product.find();
         res.status(200).json(products);
     } catch (e) {
-        res.status(400).send(e.message)
+        res.status(400).send(e.message);
     }
 })
-router.get(':/id', async(req, res) => {
-    const {id} = req.params;
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
     try {
         const product = await Product.findById(id);
-        const similar = await Product.find({category: product.category}).limit(5);
-        res.status(200).json({product, similar})
+        const similar = await Product.find({ category: product.category }).limit(5);
+        res.status(200).json({ product, similar })
     } catch (e) {
-        res.status(400).send(e.message)
+        res.status(400).send(e.message);
     }
-})
+});
+
+router.get('/category/:category', async(req,res)=> {
+    const {category} = req.params;
+    try {
+      let products;
+      const sort = {'_id': -1}
+      if(category == "all"){
+        products = await Product.find().sort(sort);
+      } else {
+        products = await Product.find({category}).sort(sort)
+      }
+      res.status(200).json(products)
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+  })
+  
 
 module.exports = router;
